79be2012c3b4ad61b4d64a63922b6508b6e5d79c
Denis Fadeev <denis@fadeev.org>
2019-10-13 14:21:36 +0500

    comment out version switcher

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var fs = require("fs");
 
 module.exports = (options, ctx) => {
-  const versions = fs
-    .readFileSync("./versions", { encoding: "utf8" })
-    .split("\n")
-    .filter(version => version != "");
-  Object.assign(
-    options,
-    Object.assign({
-      versions
-    })
-  );
-  return options;
+  // const versions = fs
+  //   .readFileSync("./versions", { encoding: "utf8" })
+  //   .split("\n")
+  //   .filter(version => version != "");
+  // Object.assign(
+  //   options,
+  //   Object.assign({
+  //     versions
+  //   })
+  // );
+  // return options;
 };
