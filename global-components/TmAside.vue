2c81c61f8fedca6633f7da5c2ad51f34c01fb6b6
Nass Donald <nassdonald@users.noreply.github.com>
2020-04-08 14:47:54 +0100

    interaction tweaks to aside banner

--- global-components/TmAside.vue
+++ global-components/TmAside.vue
@@ -36,6 +36,19 @@
   &__item
     margin-bottom .5rem
 
+    a
+      display block
+      transition transform 150ms ease-out, opacity 150ms ease-out, box-shadow 150ms ease-out
+
+      &:hover:not(:active),
+      &:focus:not(:active)
+        transform translateY(-2px)
+        opacity 0.85
+        box-shadow 0px 10px 20px rgba(0, 0, 0, 0.05), 0px 2px 6px rgba(0, 0, 0, 0.05), 0px 1px 0px rgba(0, 0, 0, 0.05)
+
+      &:active
+        transition none
+
 .search
   cursor pointer
   display flex
