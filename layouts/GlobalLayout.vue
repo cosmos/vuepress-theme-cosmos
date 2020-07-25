b07d17b6f6d758c7135d908f845c65c2dbaeb368
Cyrus Goh <hello@lovincyrus.com>
2020-07-24 17:43:44 -0700

    bind topBanner to global tm-top-banner

--- layouts/GlobalLayout.vue
+++ layouts/GlobalLayout.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div
     cookie-banner
-    tm-top-banner(v-if="this.$themeConfig.topbar.banner && this.$themeConfig.topbar.banner === true")
+    tm-top-banner(v-if="this.$themeConfig.topbar.banner && this.$themeConfig.topbar.banner === true" v-bind="{topBanner}")
     .layout
       .layout__sidebar
         tm-sidebar-content(:value="tree" :tree="directoryTree")
