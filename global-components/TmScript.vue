7c7fed16cba928784640aef6ff92a8c2e09c308d
Ibo Sy <ibo@b9lab.com>
2021-11-08 21:28:54 +0100

    tweak urls for bugherd preview

--- global-components/TmScript.vue
+++ global-components/TmScript.vue
@@ -7,7 +7,7 @@
         props: ["src", "async"],
         computed: {
             shouldIncludeScript() {
-                return window.location.origin.includes("http://preview-5bxuue6kafu5ocp5.b9lab.com/") || window.location.origin.includes("localhost:") || window.location.origin.includes("127.0.0.1");
+                return window.location.origin.includes("preview-5bxuue6kafu5ocp5.b9lab.com") || window.location.origin.includes("localhost:") || window.location.origin.includes("127.0.0.1");
             }
         }
     };
