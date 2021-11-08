f71108669f0e3e74fc5a3ab9aa35f3328c68b7ba
Ibo Sy <ibo@b9lab.com>
2021-11-08 19:43:45 +0100

    amend last commit, fix SSR for Bugherd

--- layouts/GlobalLayout.vue
+++ layouts/GlobalLayout.vue
@@ -48,7 +48,11 @@ export default {
       );
     },
     shouldIncludeScript() {
-      return (typeof window !== 'undefined') && ((window.location.origin.includes("http://preview-5bxuue6kafu5ocp5.b9lab.com/") || window.location.origin.includes("localhost:") || window.location.origin.includes("127.0.0.1"));
+      if (typeof window === 'undefined') {
+        return false;
+      } else {
+        return window.location.origin.includes("http://preview-5bxuue6kafu5ocp5.b9lab.com/") || window.location.origin.includes("localhost:") || window.location.origin.includes("127.0.0.1");
+      }
     }
   }
 };
