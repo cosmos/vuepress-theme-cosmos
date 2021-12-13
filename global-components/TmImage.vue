7c9e25f6b437a0a96cae63ad173125c08975253a
Gioele Bonifetto <gioele@b9lab.com>
2021-12-13 16:08:43 +0100

    add tm image component

---
+++ global-components/TmImage.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+    img(:src="resizedSrc" :data-zoom-src="zoomSrc")
+</template>
+
+<script>
+export default {
+    props: ['src'],
+    computed: {
+        resizedSrc() {
+            const slot = window.innerWidth > 1080 ? 'big' : 'small';
+            return '/resized-images/' + slot + this.src;
+        },
+        zoomSrc() {
+            return '' + this.src;
+        }
+    }
+}
+</script>
