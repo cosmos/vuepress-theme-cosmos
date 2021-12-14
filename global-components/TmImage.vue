a3a6a4e805ee0b4342e3d782132af3b3e275110d
Gioele Bonifetto <gioele@b9lab.com>
2021-12-14 14:06:59 +0100

    implements size based image paths logic

--- global-components/TmImage.vue
+++ global-components/TmImage.vue
@@ -7,11 +7,35 @@ export default {
     props: ['src'],
     computed: {
         resizedSrc() {
-            const slot = (typeof window !== 'undefined' && window.innerWidth > 1080) ? 'big' : 'small';
-            return '/resized-images/' + slot + this.src;
+            if (this.isAbsoluteUrl(this.src)) return this.src;
+
+            const windowWidth = (typeof window !== 'undefined') ? window.innerWidth : null;
+            const breakpoint = this.getBreakpoint(windowWidth);
+            return '/resized-images/' + breakpoint + this.src;
         },
         zoomSrc() {
-            return '' + this.src;
+            if (this.isAbsoluteUrl(this.src)) return this.src;
+            return this.src;
+        }
+    },
+    methods: {
+        isAbsoluteUrl(src) {
+            var absoluteRe = /^https?:\/\//i;
+            return absoluteRe.test(src);
+        },
+        getBreakpoint(width) {
+            var breakpoint = ''; // default
+
+            if (width && this.$themeConfig?.imageBreakpoints) {
+                for (var item of this.$themeConfig.imageBreakpoints) {
+                    if (width <= item) {
+                        breakpoint = item;
+                        break;
+                    }
+                }
+            }
+
+            return breakpoint;
         }
     }
 }
